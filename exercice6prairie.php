d93ffb33130044e4bd9cd39de1fc504c96ec2e56
loicmagny <loicmagn60@gmail.com>
2017-10-02 12:14:37 +0200

    ajout retour à la ligne

--- exercice6prairie.php
+++ exercice6prairie.php
@@ -1,12 +1,13 @@
-  <?php
-  $langage = array("HTML","CSS","Javascript","PHP");
-  var_dump ($langage);
-  sort($langage);
-  foreach ($langage as $key => $value) {
-    echo "langage[".$key."] = ".$value."\n";
-  }
-  var_dump (implode ($langage));
-  ($langage);
+<?php
+$langage = array("HTML","CSS","Javascript","PHP");
+var_dump ($langage);
+echo "<br>";
+sort($langage);
+foreach ($langage as $key => $value) {
+  echo "langage[".$key."] = ".$value."<br />\n";
+}
+var_dump (implode ($langage));
+echo "<br>";
 $fusion = $langage[0].$langage[1].$langage[2].$langage[3];
 echo strrev ($fusion);
-  ?>
+?>
